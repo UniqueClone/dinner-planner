9fc6a38f35f19b70c8cf31a58a3e78d5afa4d39d
Ryan Lynch <lynchryan2@gmail.com>
2024-09-19 15:53:56 +0100

    Adding props to TexxtInput

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -1,17 +1,44 @@
-import { TextInput } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { TextInput, View, StyleSheet, Pressable } from "react-native";
 
-export function DinnerInput() {
+export interface DinnerInputProps {
+    dinner: string;
+    setDinner: (text: string) => void;
+    onRemove: () => void;
+}
+
+export function DinnerInput(props: DinnerInputProps) {
+    const { dinner, setDinner, onRemove } = props;
+
     return (
-        <TextInput
-            placeholder="Add a dinner"
-            style={{
-                height: 40,
-                width: 200,
-                marginBottom: 12,
-                borderWidth: 1,
-                padding: 10,
-                borderRadius: 5,
-            }}
-        />
+        <View style={styles.flexContainer}>
+            <TextInput
+                placeholder="Add a dinner"
+                style={{
+                    height: 40,
+                    width: 200,
+                    marginBottom: 12,
+                    borderWidth: 1,
+                    padding: 10,
+                    borderRadius: 5,
+                }}
+                value={dinner}
+                onChangeText={setDinner}
+            />
+            <Pressable onPress={() => onRemove()}>
+                <Ionicons size={24} name="close" style={styles.icon} />
+            </Pressable>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    flexContainer: {
+        flexDirection: "row",
+    },
+    icon: {
+        color: "teal",
+        paddingLeft: 12,
+        paddingTop: 8,
+    },
+});
